f605dcd30814bbc5e97925b45ade00ca957adc87
kobasa-tech <warped.trump@gmail.com>
2021-07-18 15:30:13 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,22 @@
+// HTML部分を記述する変数を定義
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  // responseプロパティ サーバーからのレスポンス情報が格納されたプロパティ。postキーの値を受取り変数に代入
+  // postsコントローラーのcreateアクションにrender json:{post: post}の記述があるからresponseプロパティで受け取れる
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時:${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+    // 式展開でhtmlの記述。レスポンスから作成日時と内容を表示
+  return html; //レスポンスの値が入った変数htmlを戻り値として返す
+};
+
+
 function post (){ //処理名postを定義
   const submit = document.getElementById("submit");
   // idから投稿ボタンの要素を取得し変数submitに代入
@@ -28,6 +47,24 @@ function post (){ //処理名postを定義
     XHR.send(formData);
     // send()メソッド リクエストを送信するメソッド。XMLHttpRequestオブジェクトのメソッド。
     // 生成したフォームデータを送信している。
+
+    XHR.onload = () => { // onloadプロパティ レスポンスの受信に成功したときの処理を記述するプロパティ
+      if (XHR.status != 200) { // ステータスコードが200でない(リクエストが正常に完了していない)ときのの処理
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        // ポップアップでアラートを出す。ステータスコードとエラーメッセージを表示
+        return null; // JavaScriptの処理から抜け出すことができる。エラーがでた場合はこれ以降の処理は行わない。
+      };
+      const list = document.getElementById("list");
+      // idを指定して投稿されたメモ1つ1つが入るdiv要素を取得。取得した要素の直後にHTMLを挿入する
+      const formText = document.getElementById("content");
+      // メモ投稿フォームのidを取得。投稿後もフォームから投稿内容が消えない問題の解決に使用する。
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      // insertAdjacentHTMLメソッド HTMLをある要素の指定した箇所に挿入するメソッド
+      // 挿入したい要素名.insertAdjacentHTML("挿入したい位置", 挿入するHTML); afterendで要素の直後に挿入
+      // XHRを引数として変数buildXHRに渡している。
+      formText.value = "";
+      // ビューに投稿内容が反映された後に、フォームに空の文字列を代入することで中身をリセットしている
+    };
   });
 };
 
